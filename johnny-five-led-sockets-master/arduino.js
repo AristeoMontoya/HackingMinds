08a26578b0c2eb83e9f4bb548dfcc82d6dcd534b
Fer <fer@Hoddler.com>
2020-03-07 06:46:15 -0700

    Activar Relé con interruptor

---
+++ johnny-five-led-sockets-master/arduino.js
@@ -0,0 +1,27 @@
+const io = require('socket.io-client');
+const five = require('johnny-five');
+const config = require('./config');
+
+// Connect to the socket server
+const socket = io.connect(config.url);
+
+const board = five.Board();
+
+board.on('ready', function() {
+
+  const led = new five.Led(13); // Set pin 13 for LED
+
+  // Turn LED on when event led:on is received
+  socket.on('led:on', function(){
+    var relay = new five.Relay(12);
+    relay.open();
+  });
+
+  // Turn LED off when event led:off is received
+  socket.on('led:off', function(){
+    var relay = new five.Relay(12)
+    relay.close();
+
+  });
+
+});
