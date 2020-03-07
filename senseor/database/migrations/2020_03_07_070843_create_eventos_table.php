e560aa41047f3b7354f84e91e59ac49a44d38f82
otamegane2 <57646877+otamegane64@users.noreply.github.com>
2020-03-07 00:53:03 -0700

    tablas de la base de datos

---
+++ senseor/database/migrations/2020_03_07_070843_create_eventos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->char('nombre_evento',100);
+            $table->integer('Zona')->unsigned();
+            $table->integer('numero_Invitados');
+            $table->dateTime('Fecha_evento', 0);
+            $table->foreign('Zona')->references('id')->on('zonas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
