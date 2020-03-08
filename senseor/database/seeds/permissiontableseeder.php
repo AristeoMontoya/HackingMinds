9f30ab0ff213e60c6d66acbcc320d5a21f260731
Aristeo Montoya <aristeomontoya98@gmail.com>
2020-03-07 21:40:07 -0700

    Eliminados los permisos

--- senseor/database/seeds/permissiontableseeder.php
+++ senseor/database/seeds/permissiontableseeder.php
@@ -12,34 +12,6 @@ class permissiontableseeder extends Seeder
      */
     public function run()
     {
-        $permission_ids = []; // an empty array of stored permission IDs
-// iterate though all routes
-foreach (Route::getRoutes()->getRoutes() as $key => $route)
-{
- // get route action
- $action = $route->getActionname();
-// separating controller and method
- $_action = explode('@',$action);
- 
- $controller = $_action[0];
- $method = end($_action);
- 
- // check if this permission is already exists
- $permission_check = permisos::where(
-         ['controller'=>$controller,'method'=>$method]
-     )->first();
- if(!$permission_check){
-   $permission = new permisos;
-   $permission->controller = $controller;
-   $permission->method = $method;
-   $permission->save();
-   // add stored permission id in array
-   $permission_ids[] = $permission->id;
- }
-}
-// find admin role.
-$admin_role = roles::where('nombre','administrador')->first();
-// atache all permissions to admin role
-//$admin_role->permissions()->attach($permission_ids);
+        
     }
 }
